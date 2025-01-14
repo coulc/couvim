6df2a01f6a296c4b2b1e5f6450a174ac63efa3d9
CooLKingC <542587642@qq.com>
2025-01-14 15:05:44 +0800

    "修改主题"

--- nvim/lua/plugins/base/theme.lua
+++ nvim/lua/plugins/base/theme.lua
@@ -1,20 +1,20 @@
-return {
-  -- the colorscheme should be available when starting Neovim
-  "folke/tokyonight.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    -- load the colorscheme here
-    vim.cmd [[colorscheme tokyonight-moon]]
-    require("tokyonight").setup {
-      styles = {
-        -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "dark", -- style for sidebars, see below
-        floats = "dark", -- style for floating windows
-      },
-    }
-  end,
-}
+-- return {
+--   -- the colorscheme should be available when starting Neovim
+--   "folke/tokyonight.nvim",
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     -- load the colorscheme here
+--     vim.cmd [[colorscheme tokyonight-moon]]
+--     require("tokyonight").setup {
+--       styles = {
+--         -- Background styles. Can be "dark", "transparent" or "normal"
+--         sidebars = "dark", -- style for sidebars, see below
+--         floats = "dark", -- style for floating windows
+--       },
+--     }
+--   end,
+-- }
 
 -- return {
 --   "morhetz/gruvbox",
@@ -34,12 +34,11 @@ return {
 --     vim.cmd "colorscheme everforest"
 --   end,
 -- }
--- return {
---   -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
---   "catppuccin/nvim",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     vim.cmd [[colorscheme catppuccin]]
---   end,
--- }
+return {
+  "catppuccin/nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd [[colorscheme catppuccin]]
+  end,
+}
