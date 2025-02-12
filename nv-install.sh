f04c3ccc0b296ef176e42fdb760314144d519ca0
Luciuska <542587642@qq.com>
2025-02-12 11:55:15 +0000

    "install script"

--- nv-install.sh
+++ nv-install.sh
@@ -44,16 +44,16 @@ if [[ "$install_neovim" =~ ^[Yy]$ ]]; then
 fi
 
 
-echo "是否下载GeistMono Nerd Font字体? (y/n) "
+echo "是否下载JetBrainsMono字体? (y/n) "
 read install_font
 if [[ "$install_font" =~ ^[Yy]$ ]]; then
     # 下载并安装GeistMono Nerd Font
     echo "下载并安装GeistMono Nerd Font..."
-    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/GeistMono.zip
-    unzip GeistMono.zip -d /usr/share/fonts/
-    rm -rf /usr/share/fonts/README.md  /usr/share/fonts/LICENSE
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.tar.xz
+    tar -xvf JetBrainsMono.tar.xz -d /usr/share/fonts/
+    rm -rf /usr/share/fonts/README.md  /usr/share/fonts/OFL.txt
     fc-cache -fv
-    rm -rf GeistMono.zip
+    rm -rf JetBrainsMono.tar.xz
 fi
 
 
---
+++ vim-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt install  fzf python3.12-venv git vim gcc npm wget curl unzip && apt-get install ripgrep xclip
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.tar.xz
+tar -xvf JetBrainsMono.tar.xz -d /usr/share/fonts/
+rm -rf /usr/share/fonts/README.md  /usr/share/fonts/OFL.txt
+fc-cache -fv
+rm -rf JetBrainsMono.tar.xz
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
